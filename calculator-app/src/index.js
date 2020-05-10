55eab9091a2c0af56c0c4e554bb0c397902df490
rudensergey <rudensergey@gmail.com>
2020-05-10 20:03:46 +0300

    set whole project on 'create react app' rails

--- calculator-app/src/index.js
+++ calculator-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 import * as serviceWorker from './serviceWorker';
 import { StyleSheet, css } from 'aphrodite';
 

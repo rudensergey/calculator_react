297735cec0a6228a80b8945ada851bdbf275d57d
rudensergey <rudensergey@gmail.com>
2020-05-15 22:34:26 +0300

    rewrite grag'n'drop function / made it less

--- calculator-app/src/term.js
+++ calculator-app/src/term.js
@@ -6,71 +6,55 @@ export default class Term extends React.Component {
 
         this.handleMouseDrag = this.handleMouseDrag.bind(this);
         this.handleTouchDrag = this.handleTouchDrag.bind(this);
+        this.handleTerm = this.handleTerm.bind(this);
     }
 
-    handleTouchDrag(event) {
-        console.log("touch");
-
+    handleTerm(event, touch) {
         var toggle = document.getElementById("toggle");
         var timeline = document.getElementById("timeline");
-        let self = this;
-
-        event.preventDefault();
-
-        var touchLoc = event.targetTouches[0];
-        var shiftX = touchLoc.clientX - toggle.getBoundingClientRect().left;
-
-        document.addEventListener("touchmove", onMouseMove);
-        document.addEventListener("touchend", onMouseUp);
-
-        function onMouseMove(e) {
-            var touchLocation = e.targetTouches[0];
-
-            var newLeft =
-                touchLocation.clientX -
-                shiftX -
-                timeline.getBoundingClientRect().left;
-
-            var coords = timeline.getBoundingClientRect();
-
-            var point =
-                (coords.left + coords.width - 18 - (coords.left + 36)) / 345;
-
-            self.props.onChangeTerm(
-                Math.round(
-                    (touchLocation.clientX - (coords.left + 36)) / point
-                ) + 30
-            );
-
-            // курсор вышел из слайдера => оставить бегунок в его границах.
-            if (newLeft < 0) newLeft = 0;
-            var rightEdge = timeline.offsetWidth - toggle.offsetWidth;
-            if (newLeft > rightEdge) newLeft = rightEdge;
-
-            toggle.style.left = newLeft + "px";
-        }
-
-        function onMouseUp() {
-            document.removeEventListener("touchmove", onMouseMove);
-            document.removeEventListener("touchend", onMouseUp);
-        }
-    }
-
-    handleMouseDrag(event) {
-        var toggle = document.getElementById("toggle");
-        var timeline = document.getElementById("timeline");
+        var shiftX;
         const self = this;
-
         event.preventDefault();
 
-        var shiftX = event.clientX - toggle.getBoundingClientRect().left;
+        if (touch) {
+            var touchLoc = event.targetTouches[0];
+            shiftX = touchLoc.clientX - toggle.getBoundingClientRect().left;
 
-        document.addEventListener("mousemove", onMouseMove);
-        document.addEventListener("mouseup", onMouseUp);
+            document.addEventListener("touchmove", onMouseMove);
+            document.addEventListener("touchend", onMouseUp);
+
+            function onMouseUp() {
+                document.removeEventListener("touchmove", onMouseMove);
+                document.removeEventListener("touchend", onMouseUp);
+            }
+        } else {
+            shiftX = event.clientX - toggle.getBoundingClientRect().left;
+
+            document.addEventListener("mousemove", onMouseMove);
+            document.addEventListener("mouseup", onMouseUp);
+
+            function onMouseUp() {
+                document.removeEventListener("mouseup", onMouseUp);
+                document.removeEventListener("mousemove", onMouseMove);
+            }
+        }
 
         function onMouseMove(event) {
-            var newLeft =
-                event.clientX - shiftX - timeline.getBoundingClientRect().left;
+            var newLeft;
+
+            if (touch) {
+                var touchLocation = event.targetTouches[0];
+
+                newLeft =
+                    touchLocation.clientX -
+                    shiftX -
+                    timeline.getBoundingClientRect().left;
+            } else {
+                newLeft =
+                    event.clientX -
+                    shiftX -
+                    timeline.getBoundingClientRect().left;
+            }
 
             var coords = timeline.getBoundingClientRect();
 
@@ -87,11 +71,14 @@ export default class Term extends React.Component {
 
             toggle.style.left = newLeft + "px";
         }
+    }
 
-        function onMouseUp() {
-            document.removeEventListener("mouseup", onMouseUp);
-            document.removeEventListener("mousemove", onMouseMove);
-        }
+    handleTouchDrag(event) {
+        this.handleTerm(event, true)
+    }
+
+    handleMouseDrag(event) {
+        this.handleTerm(event, false)
     }
 
     render() {

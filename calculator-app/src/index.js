e82e5b3f9476e77c71483d8dba0fcdfc586859fb
rudensergey <rudensergey@gmail.com>
2020-06-01 15:28:58 +0300

    add switcher button

--- calculator-app/src/index.js
+++ calculator-app/src/index.js
@@ -3,24 +3,40 @@ import ReactDOM from "react-dom";
 import Calculator from "./calculator.js";
 import { ThemeContext } from "./themecontext.js";
 
+import "./style/style.css";
+import "./style/calculator.css";
+import "./style/mobile.css";
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { theme: "light" };
     }
 
-    componentDidMount = () => {
-        setInterval(() => {
-            this.state.theme === "light"
-                ? this.setState({ theme: "dark" })
-                : this.setState({ theme: "light" });
-        }, 2000);
+    changeTheme = () => {
+        this.state.theme === "light"
+            ? this.setState({ theme: "dark" })
+            : this.setState({ theme: "light" });
     };
 
     render() {
         return (
             <ThemeContext.Provider value={this.state.theme}>
                 <Calculator />
+                <div
+                    style={{
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                >
+                    <button
+                        className="theme-button"
+                        onClick={this.changeTheme}
+                    >
+                        Поменять тему
+                    </button>
+                </div>
             </ThemeContext.Provider>
         );
     }
